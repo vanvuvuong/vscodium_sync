7cbe42f3a2932b89e1ccda879f16f329bd56de77
vanvuvuong <kris@DongTD-D8>
2023-09-12 15:33:42 +0700

    sync user setting data

---
+++ scripts/extension.sh
@@ -0,0 +1,5 @@
+codium --install-extension Updating VS Code Server to version d48b950f7741008f7fb375881b45188dd73ecac4
+codium --install-extension Removing previous installation...
+codium --install-extension Installing VS Code Server for x64 (d48b950f7741008f7fb375881b45188dd73ecac4)
+codium --install-extension Downloading: 100%
+codium --install-extension Failed
